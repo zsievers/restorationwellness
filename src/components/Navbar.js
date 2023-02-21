3874c8640f915131b1403209ddabbac2744036ee
Zach Sievers <zach.sievers@serrala.com>
2023-02-21 12:50:47 -0500

    clearn up p2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -61,7 +61,7 @@ export default function Example() {
 
   return (
     <Navbar className="sticky top-0 z-50 mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4">
-      <div className="container text-gray-600 mx-auto flex items-center justify-between text-blue-gray-900">
+      <div className="container text-gray-600 mx-auto flex items-center justify-between">
         <Typography
           as="a"
           href="#"
