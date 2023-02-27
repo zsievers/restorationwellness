6131ac5422d03fe06297dbf916b1b9eefc6a3367
Zach Sievers <zach.sievers@serrala.com>
2023-02-27 11:37:59 -0500

    edit hero img

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,14 +9,14 @@ export default function Header() {
           alt="lavendar field"
           className="w-full h-full object-cover absolute m-auto mix-blend-overlay"
         />
-        <div className="flex flex-col text-center px-10 py-5 mb-10">
+        {/* <div className="flex flex-col text-center px-10 py-5 mb-10">
           <h1 className="text-black text-5xl md:text-7xl lg:text-8xl font-light font-logo2 mt-10">
             Restoration Wellness
           </h1>
           <h2 className="text-black text-xl md:text-4xl lg:text-5xl font-light font-logo2 mt-5">
             Hope | Confidence | Freedom
           </h2>
-        </div>
+        </div> */}
         <div className="flex flex-col px-8 py-4 mb-10 md:py-2 border-4 bg-white w-3/4 m-auto opacity-75">
           <p className="text-black text-center text-lg md:text-2xl lg:3xl font-light font-logo1">
             I believe that every person should feel FREE in their bodies:
